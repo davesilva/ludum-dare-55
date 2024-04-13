1d329dc98e3c225d3ba5af7e73b16819cfceaf50
jcoates <git@jcoat.es>
2024-04-13 13:13:02 -0400

    Add the folder base structure for rooms.

---
+++ project/room/room.gd
@@ -0,0 +1,18 @@
+extends Node2D
+
+
+# Declare member variables here. Examples:
+export var cleanliness := 0
+var naughtyLevel := 0
+var niceLevel := 0
+enum ROOM_STATE {CLEAN, DIRTY, RUINED}
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
