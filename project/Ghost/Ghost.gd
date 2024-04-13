c4db7c3a71d624cc32f1e6480d3afc32e59bfe6d
Dave Silva <github@dmsilva.com>
2024-04-13 16:26:19 -0400

    Ghost movement

--- project/Ghost/Ghost.gd
+++ project/Ghost/Ghost.gd
@@ -5,21 +5,27 @@ export (int) var mood = 100
 export (float) var movement_speed = 1
 export (float) var chore_speed = 1
 
+var selected = false
+
 var destination: Chamber
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass
 
-func _setup_signals():
-	# Connect send_to_location signal
-	pass
+func _on_send_to_location(destination: Chamber):
+	if !self.selected:
+		return
 
-func _on_send_to_location(destination: SpookyRoom):
 	self.destination = destination
-	var tween = create_tween()
-	tween.tween_property(self, "position", destination, 1)
-	tween.connect("finished", self, "_on_arrived_at_location")
+	var distance = self.position.distance_to(destination.position)
+	var duration = distance / (self.movement_speed * 100)
 
-func _on_arrived_at_location():
-	pass
+	var tween = create_tween()
+	tween.tween_property(self, "position", destination, duration)
+
+func _on_selected():
+	self.selected = true
+
+func _on_unselected():
+	self.selected = false
