688ab2066acf12cb404c4b61f8e981d256d5403f
Dave Silva <github@dmsilva.com>
2024-04-14 10:41:16 -0400

    Make ghost move good

--- project/Ghost/Ghost.gd
+++ project/Ghost/Ghost.gd
@@ -2,8 +2,8 @@ extends Node2D
 class_name Ghost
 
 export (int) var mood = 100
-export (float) var movement_speed = 1.0
-export (float) var chore_speed = 1.0
+export (float) var movement_speed = 1
+export (float) var chore_speed = 1
 
 var selected = false
 
@@ -14,16 +14,20 @@ var current_location
 func _ready():
 	add_to_group(Constants.GROUP_GHOST)
 
-func _on_send_to_location(send_destination):
-	if !self.selected:
+func send_to_location(sent_destination):
+	var room_destination = sent_destination as Node2D
+	if not room_destination:
 		return
 
-	self.destination = send_destination
-	var distance = self.position.distance_to(send_destination.position)
+	if not self.selected:
+		return
+
+	self.destination = room_destination
+	var distance = self.position.distance_to(room_destination.position)
 	var duration = distance / (self.movement_speed * 100)
 	print(distance)
 	var tween = create_tween()
-	tween.tween_property(self, "position", send_destination, duration)
+	tween.tween_property(self, "position", room_destination.position, duration)
 
 func _on_selected():
 	self.selected = true
