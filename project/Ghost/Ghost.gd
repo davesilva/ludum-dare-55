39c66a8f29f44a80ffa2a136900d5c6e66994928
jcoates <git@jcoat.es>
2024-04-14 11:47:21 -0400

    Add area to ghost
    (Also starts the work i'm doing because it's cursed to disentangle)

--- project/Ghost/Ghost.gd
+++ project/Ghost/Ghost.gd
@@ -1,11 +1,15 @@
 extends Node2D
 class_name Ghost
 
+enum STATE {RAGING, CLEANING, IDLE, TRAVELING}
+
 export (int) var mood = 100
 export (float) var movement_speed = 1.0
 export (float) var chore_speed = 1.0
 
 var selected = false
+# PRETEND THIS IS A GHOST_STATE
+var state: int = STATE.IDLE
 
 var destination_info: SpookyRoomInfo
 var current_location_info: SpookyRoomInfo
@@ -26,6 +30,11 @@ func send_to_location(send_destination_info: SpookyRoomInfo):
 	var tween = create_tween()
 	tween.tween_property(self, "position", destination_info.global_position, duration)
 
+func is_happy():
+	return self.mood >= 0
+	
+func is_angry():
+	return self.mood < 0
 
 func _on_selected():
 	self.selected = true
--- project/spooky_room/spooky_room.gd
+++ project/spooky_room/spooky_room.gd
@@ -6,20 +6,21 @@ class_name SpookyRoom
 # TODO: make a resource(?) 
 export var cleanImage: Texture
 export var dirtyImage: Texture
+export var ruinedImage: Texture
 export var roomWidth: int
 export var roomHeight: int
 export var processSpeedInSeconds: int = 5
+export var roomHasTask: bool = true
 
 # These ones are generic
 export var dirtiness := 0
-var naughtyLevel := 0
-var niceLevel := 0
 enum ROOM_STATE {CLEAN, DIRTY, RUINED}
 onready var sprite = $Sprite
 onready var room_collision_shape_2d = $RoomArea2D/RoomCollisionShape2D
 
 var room_info: SpookyRoomInfo
-
+# NOTE: Change this if we commit for sure to just one ghost
+var present_ghosts: Dictionary = {}
 
 # Converts the int to the state (real return type is ROOM_STATE)
 func dirtinessToState(c: int) -> int:
@@ -43,28 +44,42 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	# TODO: Remove this
-	if Input.is_action_pressed("ui_up") and dirtiness > 0:
-		niceLevel = 1
-		naughtyLevel = 0
-	elif Input.is_action_pressed("ui_down") and dirtiness < processSpeedInSeconds:
-		niceLevel = 0
-		naughtyLevel = -1
+	if Input.is_action_just_pressed("ui_up") and dirtiness > 0:
+		dirtiness -= processSpeedInSeconds/4.0
+		print(dirtiness)
+	elif Input.is_action_just_pressed("ui_down") and dirtiness < processSpeedInSeconds:
+		dirtiness += processSpeedInSeconds/4.0
+		print(dirtiness)
 
-	var progress = (delta*((-1)*(niceLevel+naughtyLevel)))
-	dirtiness += progress
+	if roomHasTask:
+		# NOTE: This can be simplified a lot if we only have one ghost
+		var helpful_ghosts = []
+		var naughty_ghosts = []
+		for ghost in present_ghosts:
+			if ghost.is_happy() and ghost.state != Ghost.STATE.Traveling:
+				helpful_ghosts.append(ghost)
+			elif ghost.is_angry() and ghost.state != Ghost.STATE.Traveling:
+				naughty_ghosts.append(ghost)
+		var progress = 0
+		for ghost in helpful_ghosts:
+			progress += ghost.chore_speed
+		for ghost in naughty_ghosts:
+			progress -= ghost.chore_speed
+		progress = progress * delta
+		dirtiness -= progress
 
-	if dirtiness > processSpeedInSeconds:
-		dirtiness = processSpeedInSeconds
-	elif dirtiness < 0:
-		dirtiness = 0
+		if dirtiness > processSpeedInSeconds:
+			dirtiness = processSpeedInSeconds
+		elif dirtiness < 0:
+			dirtiness = 0
 
-	match dirtinessToState(dirtiness):
-		ROOM_STATE.CLEAN:
-			sprite.texture = cleanImage
-		ROOM_STATE.DIRTY:
-			sprite.texture = dirtyImage
-		ROOM_STATE.RUINED:
-			sprite.texture = dirtyImage
+		match dirtinessToState(dirtiness):
+			ROOM_STATE.CLEAN:
+				sprite.texture = cleanImage
+			ROOM_STATE.DIRTY:
+				sprite.texture = dirtyImage
+			ROOM_STATE.RUINED:
+				sprite.texture = ruinedImage
 
 func _on_RoomArea2D_body_entered(body):
 	if body.is_in_group(Constants.GROUP_PLAYER):
@@ -82,3 +97,15 @@ func _on_RoomArea2D_input_event(_viewport, event, _shape_idx):
 
 func _on_RoomArea2D_body_exited(_body):
 	pass
+
+
+func _on_GhostActiveArea2D_area_entered(area):
+	if area.is_in_group(Constants.GROUP_GHOST):
+		var ghost = area as Ghost
+		self.present_ghosts[ghost] = null
+
+
+func _on_GhostActiveArea2D_area_exited(area):
+	if area.is_in_group(Constants.GROUP_GHOST):
+		var ghost = area as Ghost
+		self.present_ghosts.erase(ghost)
