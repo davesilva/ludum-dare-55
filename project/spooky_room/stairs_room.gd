e2cb21c1449723395d9af0f183102805d9249fb0
jcoates <git@jcoat.es>
2024-04-14 18:53:56 -0400

    Whoops missed all the stairs files

---
+++ project/spooky_room/stairs_room.gd
@@ -0,0 +1,30 @@
+extends SpookyRoom
+class_name StairsRoom
+
+export onready var spawn_point: Vector2 =  Vector2(self.global_position.x, self.global_position.y+34)
+export (NodePath) var target_room_path
+
+func _ready():
+	._ready()
+	GlobalSignals.connect("player_takes_stairs", self, "_on_player_takes_stairs")
+
+func _on_RoomArea2D_body_entered(body):
+	._on_RoomArea2D_body_entered(body)
+	var other_room = get_node(target_room_path) as Node2D
+	var player = body as PlayerCharacter
+	player.available_action = PlayerCharacter.PlayerActions.CLIMB_STAIRS
+	player.stairs_target = other_room
+	
+func _on_RoomArea2D_body_exited(body):
+	._on_RoomArea2D_body_exited(body)
+	if body.is_in_group(Constants.GROUP_PLAYER):
+		var player = body as PlayerCharacter
+		player.stairs_target = null
+		player.available_action = PlayerCharacter.PlayerActions.NONE
+		
+func _on_player_takes_stairs(target_room, player):
+	if self != target_room:
+		return
+	player.position = spawn_point
+	# NOTE: this is where a fade in animation would go if we did that
+		
