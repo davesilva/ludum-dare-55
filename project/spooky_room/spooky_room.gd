9f70f29b4ddfcc65daefcba5e88202477129e322
jcoates <git@jcoat.es>
2024-04-14 13:53:20 -0400

    Add dirtier image, resize room hitboxes

--- project/spooky_room/spooky_room.gd
+++ project/spooky_room/spooky_room.gd
@@ -6,6 +6,7 @@ class_name SpookyRoom
 # TODO: make a resource(?) 
 export var cleanImage: Texture
 export var dirtyImage: Texture
+export var dirtierImage: Texture
 export var ruinedImage: Texture
 export var roomWidth: int
 export var roomHeight: int
@@ -13,10 +14,8 @@ export var processSpeedInSeconds: int = 5
 export var roomHasTask: bool = true
 
 # These ones are generic
-export var dirtiness := 0
-var naughtyLevel := 0
-var niceLevel := 0
-enum ROOM_STATE {CLEAN, DIRTY, RUINED}
+export var dirtiness := 0.0
+enum ROOM_STATE {CLEAN, DIRTY, DIRTIER, RUINED}
 onready var sprite = $Sprite
 onready var room_collision_shape_2d = $RoomArea2D/RoomCollisionShape2D
 
@@ -28,10 +27,12 @@ var present_ghosts: Dictionary = {}
 
 # Converts the int to the state (real return type is ROOM_STATE)
 func dirtinessToState(c: int) -> int:
-	if c < processSpeedInSeconds/2.0:
+	if c < processSpeedInSeconds/3.0:
 		return ROOM_STATE.CLEAN
-	elif c < processSpeedInSeconds:
+	elif c < (processSpeedInSeconds) * 2/3.0:
 		return ROOM_STATE.DIRTY
+	elif c < processSpeedInSeconds:
+		return ROOM_STATE.DIRTIER
 	else:
 		return ROOM_STATE.RUINED
 
@@ -40,20 +41,21 @@ func _ready():
 	add_to_group(Constants.GROUP_ROOM)
 	sprite.texture = cleanImage
 	
+	if not roomWidth:
+		roomWidth = sprite.texture.get_width()
+	if not roomHeight:
+		roomHeight = sprite.texture.get_height()
 	room_info = SpookyRoomInfo.new(self.global_position)
-	
-	# room_collision_shape_2d.shape.set_extents(Vector2(roomWidth, roomHeight))
+	room_collision_shape_2d.shape.set_extents(Vector2(roomWidth/2.0, roomHeight/2.0))
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	# TODO: Remove this
 	if Input.is_action_pressed("ui_up") and dirtiness > 0:
-		niceLevel = 1
-		naughtyLevel = 0
+		dirtiness -= processSpeedInSeconds/4.0
 	elif Input.is_action_pressed("ui_down") and dirtiness < processSpeedInSeconds:
-		niceLevel = 0
-		naughtyLevel = -1
-
+		dirtiness += processSpeedInSeconds/4.0
+		
 	if roomHasTask:
 		# NOTE: This can be simplified a lot if we only have one ghost
 		var helpful_ghosts = []
@@ -71,18 +73,20 @@ func _process(delta):
 		progress = progress * delta
 		dirtiness -= progress
 
-	if dirtiness > processSpeedInSeconds:
-		dirtiness = processSpeedInSeconds
-	elif dirtiness < 0:
-		dirtiness = 0
+		if dirtiness > processSpeedInSeconds:
+			dirtiness = processSpeedInSeconds
+		elif dirtiness < 0:
+			dirtiness = 0
 
-	match dirtinessToState(dirtiness):
-		ROOM_STATE.CLEAN:
-			sprite.texture = cleanImage
-		ROOM_STATE.DIRTY:
-			sprite.texture = dirtyImage
-		ROOM_STATE.RUINED:
-			sprite.texture = dirtyImage
+		match dirtinessToState(dirtiness):
+			ROOM_STATE.CLEAN:
+				sprite.texture = cleanImage
+			ROOM_STATE.DIRTY:
+				sprite.texture = dirtyImage
+			ROOM_STATE.DIRTIER:
+				sprite.texture = dirtierImage
+			ROOM_STATE.RUINED:
+				sprite.texture = ruinedImage
 
 func _on_RoomArea2D_body_entered(body):
 	if body.is_in_group(Constants.GROUP_PLAYER):
