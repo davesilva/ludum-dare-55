6673e8dde473c71abeac315d079ab0fdffa15e4d
Dave Silva <github@dmsilva.com>
2024-04-15 19:52:08 -0400

    Add title screen

--- project/contexts/title_context/title_context_ui.gd
+++ project/contexts/title_context/title_context_ui.gd
@@ -5,3 +5,11 @@ signal request_context_change(context_id)
 
 func _on_start_pressed():
 	emit_signal("request_context_change", GameContext.CONTEXT_ID)
+
+
+func _on_start_mouse_entered():
+	$StartButton.rect_scale = Vector2(1.2, 1.2)
+
+
+func _on_start_mouse_exited():
+	$StartButton.rect_scale = Vector2(1.0, 1.0)
