529a1e136eda9fd1e31009606d1a357ebfe55ecb
Dave Silva <github@dmsilva.com>
2024-04-15 19:25:24 -0400

    Add janky ghost spawn timer

--- project/House/House.gd
+++ project/House/House.gd
@@ -1,5 +1,7 @@
 extends Node2D
 
+onready var ghost_spawn_timer = $GhostSpawnTimer
+var ghost_scene: PackedScene
 
 # Declare member variables here. Examples:
 # var a = 2
@@ -8,10 +10,21 @@ extends Node2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	randomize()
 	$BG.play()
-	pass
+	ghost_spawn_timer.connect("timeout", self, "_on_spawn_ghost")
+	ghost_scene = preload("res://project/Ghost/Ghost.tscn")
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func _on_spawn_ghost():
+	print("spawn ghost")
+	var rooms: Array = get_tree().get_nodes_in_group(Constants.GROUP_ROOM)
+	rooms.shuffle()
+	for room in rooms:
+		if room.roomHasTask and not room.dirtinessToState(room.dirtiness) == SpookyRoom.ROOM_STATE.RUINED and room.present_ghosts.empty():
+			var ghost: Ghost = ghost_scene.instance()
+			ghost.mood = -100
+			ghost.position = Vector2(-100, 540)
+			add_child(ghost)
+			ghost.send_to_location(room.room_info, false)
+			return
+	# If we don't have a valid place for it, don't spawn the ghost
