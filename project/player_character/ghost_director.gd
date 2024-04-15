9e0e9d82dfd73f94593c20aad4ccb5cde970a83d
jcoates <git@jcoat.es>
2024-04-14 22:59:36 -0400

    Don't allow player to send ghosts to rooms that have no tasks.

--- project/player_character/ghost_director.gd
+++ project/player_character/ghost_director.gd
@@ -11,7 +11,18 @@ func _on_room_clicked(clicked_room: SpookyRoom) -> void:
 	for room in get_tree().get_nodes_in_group(Constants.GROUP_ROOM):
 		room = room as SpookyRoom
 		if room.contains_player:
-			for ghost in room.present_ghosts:
-				ghost = ghost as Ghost
-				if ghost.state == Ghost.STATE.IDLE or ghost.state == Ghost.STATE.CLEANING:
-					ghost.send_to_location(clicked_room.room_info)
+			if not clicked_room.roomHasTask:
+				return
+			var any_happy_ghosts = false
+			for g in clicked_room.present_ghosts:
+				if g.mood >= 0:
+					any_happy_ghosts = true
+					continue
+			if not any_happy_ghosts:
+				for ghost in room.present_ghosts:
+					ghost = ghost as Ghost
+					if ghost.state == Ghost.STATE.IDLE or ghost.state == Ghost.STATE.CLEANING:
+						ghost.send_to_location(clicked_room.room_info)
+			else:
+				#This is where you would play a "NO GOOD" sound
+				pass
